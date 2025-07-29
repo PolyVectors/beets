c31c821caabc8b9160585a21e2ac68500fabc168
PolyVectors <PolyVector@protonmail.com>
2025-07-29 02:25:46 +0100

    REALLY REALLY basic binding generator

---
+++ bindings/generate.rb
@@ -0,0 +1,89 @@
+USAGE = "Usage: generate INFILE OUTFILE"
+
+TYPE_CONVERSIONS = {
+    "char" => "c::char",
+    "int" => "i32",
+    "unsigned" => "u32",
+    "size_t" => "size",
+    "int64_t" => "i64",
+}
+
+module Definition
+    NONE = 0
+    STRUCT = 1
+    ENUM = 2
+    FUNCTION = 3
+end
+
+if ARGV.length != 2
+    abort(USAGE)
+end
+
+def convert_type(type, is_pointer)
+    type = is_pointer ? type = type.prepend('*') : type
+
+    if TYPE_CONVERSIONS.has_key?(type) then
+        return TYPE_CONVERSIONS[type]
+    elsif type.start_with?("enum") or type.start_with?("struct")
+        return type.split.last
+    end
+
+    if type == "*void"
+        return "*opaque"
+    end
+
+    return type
+end
+
+infile = File.open(ARGV[0])
+
+current_defined = Definition::NONE
+outfile_contents = ""
+
+infile.read.split("\n").each_with_index { |line, index|
+    line = line.strip
+    next if line.start_with?("/*", "*/", "*") or line.empty?
+
+    if line.include?("struct") && line.include?("{")
+        current_defined = Definition::STRUCT
+    elsif line.include?("enum") && line.include?("{")
+        current_defined = Definition::ENUM
+    end
+
+    case current_defined
+    when Definition::STRUCT
+        case line
+        when /struct/
+            outfile_contents << "export type #{line.split[1]} = struct {\n"
+        when /};/
+            outfile_contents << line + "\n\n"
+        else
+            name = line.split.last.chomp(';')
+            is_pointer = name.start_with?('*')
+            name = is_pointer ? name = name[1..-1] : name
+
+            type = convert_type(line.split[0..-2].join(' '), is_pointer)
+            outfile_contents << "        #{name}: #{type},\n"
+        end
+    when Definition::ENUM
+        case line
+        when /enum/
+            outfile_contents << "export type #{line.split[1]} = enum {\n"
+        when /};/
+            outfile_contents << line + "\n\n"
+        else
+            outfile_contents << "        #{line}\n"
+        end
+    end 
+
+    if line.include?("};")
+        current_defined = Definition::NONE
+    end
+}
+infile.close()
+
+outfile_contents = outfile_contents.strip
+
+outfile = File.new(ARGV[1], "w")
+outfile.write(outfile_contents)
+outfile.close()
