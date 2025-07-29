77f6fb1f38f5af2177725a2db188c51194d35402
PolyVectors <PolyVector@protonmail.com>
2025-07-29 14:15:55 +0100

    improve binding generation, prepare to write struct member generation

--- bindings/generate.rb
+++ bindings/generate.rb
@@ -4,14 +4,6 @@ end
 
 USAGE = "Usage: generate INFILE OUTFILE"
 
-TYPE_CONVERSIONS = {
-    "char" => "c::char",
-    "int" => "i32",
-    "unsigned" => "u32",
-    "size_t" => "size",
-    "int64_t" => "i64",
-}
-
 module Definition
     NONE = 0
     STRUCT = 1
@@ -19,27 +11,52 @@ module Definition
     FUNCTION = 3
 end
 
-def convert_type(type, is_pointer)
-    type = type.prepend('*') if is_pointer
-    type = "*opaque" if type == "*void"
-
-    if TYPE_CONVERSIONS.has_key?(type) then
-        return TYPE_CONVERSIONS[type]
-    elsif type.start_with?("enum") or type.start_with?("struct")
-        return type.split.last
+def convert_type(name, type, is_pointer)
+    case type
+    when "int"
+        type = "i32"
+    when "unsigned"
+        type = "u32"
+    when "char"
+        type = "c::char"
     end
 
-    return type
+    type = type.prepend('*') if is_pointer
+    type = "*opaque" if type == "*void"
+    
+    if type.start_with?("enum") or type.start_with?("struct")
+        type = type.split.last
+        return "\t#{name}: #{type},\n"
+    end
+
+    if name.include?("[")
+        type.prepend(name.split("[")[1..-1].join("[").prepend("["))
+        name = name.split("[").first
+    end
+
+    return "\t#{name}: #{type},\n"
+end
+
+def convert_struct
+
 end
 
 infile = File.open(ARGV[0])
 
 current_defined = Definition::NONE
+
+formatted_infile = Array.new
 outfile_contents = ""
 
-infile.read.split("\n").each_with_index { |line, index|
+infile.read.split("\n").each_with_index{ |line|
     line = line.strip
     next if line.start_with?("/*", "*/", "*") or line.empty?
+    formatted_infile << line + "\n"
+}.join("\n")
+
+index = 0
+while index < formatted_infile.length do
+    line = formatted_infile[index].strip
 
     if line.include?("struct") && line.include?("{")
         current_defined = Definition::STRUCT
@@ -50,31 +67,41 @@ infile.read.split("\n").each_with_index { |line, index|
     case current_defined
     when Definition::STRUCT
         case line
-        when /struct/
-            outfile_contents << "export type #{line.split[1]} = struct {\n"
-        when /};/
+        when /^struct/
+            if line[-1] == ";"
+                name = line.split.last[0..-2]
+                is_pointer = name.start_with?('*')
+                name = name[1..-1] if is_pointer
+
+                type = line.split[1]
+                outfile_contents << "\t#{name}: #{"*" if is_pointer}#{type},\n"
+            else
+                outfile_contents << "export type #{line.split[1]} = struct {\n"
+            end
+        when /^};$/
             outfile_contents << line + "\n\n"
         else
             name = line.split.last.chomp(';')
             is_pointer = name.start_with?('*')
             name = name[1..-1] if is_pointer
 
-            type = convert_type(line.split[0..-2].join(' '), is_pointer)
-            outfile_contents << "\t#{name}: #{type},\n"
+            outfile_contents <<  convert_type(name, line.split[0..-2].join(' '), is_pointer)
         end
     when Definition::ENUM
         case line
-        when /enum/
+        when /^enum/
             outfile_contents << "export type #{line.split[1]} = enum {\n"
-        when /};/
-            outfile_contents << line + "\n\n"
+        when /^};$/
+            outfile_contents << line + "\n"
         else
-            outfile_contents << "\t#{line}\n"
+            outfile_contents << "\t#{line}"
         end
     end 
 
     current_defined = Definition::NONE if line.start_with?("};")
-}
+    index += 1
+end
+
 outfile_contents = outfile_contents.strip
 infile.close()
 
